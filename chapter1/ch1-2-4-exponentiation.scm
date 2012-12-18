b5c58abf76c0d68a034326308aeb48d8e32ff8eb
Lambda Adi <Uplus03bb@gmail.com>
2012-12-18 18:37:37 +0530

    Added warning about a source I referenced

---
+++ chapter1/ch1-2-4-exponentiation.scm
@@ -0,0 +1,16 @@
+; Chapter 1.2.4. Exponentiation - Linear Recursive PROCESS
+; Given that b^n = b.b^(n - 1) and b^0 = 1
+; Directly translated into procedure
+
+(load-option 'format) ; Invokes library to format text
+
+(define (expt b n)
+    (display "b is ")
+	(display b)
+	(display " count n is ")
+	(display n)
+	(format #t "~%")
+    (if (= n 0)
+		1
+		(* b (expt b (- n 1)))))
+	
