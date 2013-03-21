52382176c0a53da6aa6d3077fa7fe685219f44c9
lambdadi <lambdadi@gmail.com>
2013-03-21 17:33:06 +0530

    Fixed minor formatting and commenting errors

--- chapter1/ex1-12-pascals-triangle.scm
+++ chapter1/ex1-12-pascals-triangle.scm
@@ -7,8 +7,10 @@
 	(if (OR (= n 0) (= x 0) (= x n))
 		1
 		(+ (the-digit (- x 1) (- n 1)) (the-digit x (- n 1)))))
-		; This generates a Recursive Tree process
+		; This generates a Tree-Recursive  process
 
+; Print all digits of the entire nth row
+; i.e. all binomial coefficients for (x + y)^n
 (define (pascal-row n)
 	(define (walk-row count)
 		(display (the-digit count n))
@@ -16,10 +18,11 @@
 		(if (<= count 0) 1 (walk-row (- count 1))))
 	(walk-row n))
 
+; Construct a complete pascal triangle
+; Print all rows for coefficients from n = 0 to n
 (define (pascal-triangle n)
 	(define (walk-down count)
 		(pascal-row count)
 		(format #t "~%")
 		(if (>= count n) 1 (walk-down (+ count 1))))
 	(walk-down 0))
-
