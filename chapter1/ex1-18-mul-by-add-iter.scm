4219d9f24e45352a64c372e64c09f29f62f34c3b
lambdadi <lambdadi@gmail.com>
2013-03-21 17:39:20 +0530

    Exercise 1.18
    Ex 1.18 Iterative process to multiply using addition
    The process to multiply, using only addition, grows by theta(log n)
    steps, in constant space.

---
+++ chapter1/ex1-18-mul-by-add-iter.scm
@@ -0,0 +1,31 @@
+; Ex 1.18 Iterative process to multiply using addition
+    ; The process to multiply, using only addition, grows by 
+    ; theta(log n) steps, in constant space.
+
+(define (* a b)
+  ; I display object '*' ONLY as a quick-n-dirty way to visually 
+  ; verify order of growth.
+  (display *)
+  (display " ")
+  (cond ((= b 0) 0)
+        ((= b 1) a)
+        ((halve b) (* (double a) (halve b)))
+        (else (+ a (* a (- b 1))))))
+
+        
+; (halve) as a black-box abstraction
+    ; Strictly speaking, one can argue that division and remainder 
+    ; are illegal primitives based on the wording of the question. 
+    ; However, since I use halve as a black-box abstraction, 
+    ; I take the liberty to assume that its internal definition 
+    ; can be easily substituted by one of the well-understood 
+    ; division algorithm that operate using only primitive addition
+    ; and subtraction.
+(define (halve x)
+  (if (= (remainder x 2) 0)
+      (/ x 2)
+      #f))
+
+      
+; double in terms of primitive addition
+(define (double x) (+ x x))
