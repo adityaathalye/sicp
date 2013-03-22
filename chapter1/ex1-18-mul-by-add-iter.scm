335dad374367dec061a26782213d082aa9cd3f7c
lambdadi <lambdadi@gmail.com>
2013-03-22 16:12:25 +0530

    Rewrite procedure to iterate with accumulator
    I rewrote the procedure to explicitly use an accumulator as a state
    variable. The previous procedure definition was succinct, and I liked it
    too much, so I moved that one into the ex1-17-mul-by-fast-add.scm file.

--- chapter1/ex1-18-mul-by-add-iter.scm
+++ chapter1/ex1-18-mul-by-add-iter.scm
@@ -1,17 +1,20 @@
 ; Ex 1.18 Iterative process to multiply using addition
     ; The process to multiply, using only addition, grows by 
     ; theta(log n) steps, in constant space.
+        
+(define(* a b)
+  (if (= b 0) 
+    0 
+    (mult-iter 0 a b)))
 
-(define (* a b)
-  ; I display object '*' ONLY as a quick-n-dirty way to visually 
-  ; verify order of growth.
-  (display *)
-  (display " ")
-  (cond ((= b 0) 0)
-        ((= b 1) a)
-        ((halve b) (* (double a) (halve b)))
-        (else (+ a (* a (- b 1))))))
-
+(define (mult-iter mult-sum a b)
+    ; I display object '*' ONLY as a quick-n-dirty way to visually 
+    ; verify order of growth.
+    (display (+ mult-sum a))
+    (display " ")
+    (cond ((= b 1) (+ mult-sum a))
+        ((halve b) (mult-iter mult-sum (double a) (halve b)))
+        (else (mult-iter (+ mult-sum a) a (- b 1)))))
         
 ; (halve) as a black-box abstraction
     ; Strictly speaking, one can argue that division and remainder 
