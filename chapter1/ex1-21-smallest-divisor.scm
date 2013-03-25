730bda03f67e172c10b8e2f1e87befc467dc59a8
lambdadi <lambdadi@gmail.com>
2013-03-25 17:56:22 +0530

    Use script to verify hand calculations for Ex1-21

---
+++ chapter1/ex1-21-smallest-divisor.scm
@@ -0,0 +1,15 @@
+; Ex 1.21 Smallest Divisor
+
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+
+(define (square x) (* x x))
+
+(define (divides? x y)
+  (= (remainder x y) 0))
