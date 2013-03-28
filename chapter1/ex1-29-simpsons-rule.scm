2c38b541e85514c974c5afd0a4c72fc8b25057ec
lambdadi <lambdadi@gmail.com>
2013-03-28 22:58:08 +0530

    Less repeat computation, cleanup code, comment it
    Got the idea for restructuring the conditional inside (term x) from
    answer in the Scheme wiki... very nice. Also computing n-even once
    should(?) reduce the redundancy of computation.

--- chapter1/ex1-29-simpsons-rule.scm
+++ chapter1/ex1-29-simpsons-rule.scm
@@ -4,36 +4,39 @@
 ; SO GLAD I did NOT peek :)))
 
 (define (simpsons-rule f a b n)
+    ; n must always be even
+    (define n-even
+        (if (even? n) n (+ n 1)))
 
-    (define (next m) (+ m h))
-
+    ; h is the smallest increment
     (define h 
-        (/ (- b a) (evenify n)))
+        (/ (- b a) n-even))
 
-    (define (term x) 
-        (cond ((= (/ x h) (evenify n))
-                (* 1.0 (f (+ a x))))
-              ((and (even? (/ x h)) (<= (/ x h) (evenify n)))
-                (* 2.0 (f (+ a x))))
-              (else
-                (* 4.0 (f (+ a x))))))
-                
+    ; Apply the correct multiplier to each yk to create 
+    ; a single summable term
+    (define (term x)
+        (* (cond ((= (/ x h) n-even) 1.0)
+                 ((even? (/ x h)) 2.0)
+                 (else 4.0))
+           (f (+ a x))))
+           
+    ; The transform that 'sum' uses to increment each new yk term
+    (define (next m) 
+        (+ m h))
+
+    ; Invoke the computation using the 'sum' procedure. Note that 
+    ; the 0-th term is added as the logic starts computing from
+    ; the 1-st term.
     (if (<= n 0) 
         "Cannot compute"
         (* (/ h 3.0) (+ (f a) (sum term h next (- b a))))))
 
-    
+; Sum of series limited by x and y
 (define (sum term x next y)
     (if (> x y)
         0
         (+ (term x)
            (sum term (next x) next y))))
     
-    
-(define (evenify x)
-    (if (even? x) x (+ x 1)))
-
-    
 (define (even? x)
     (= (remainder x 2) 0))
-    
