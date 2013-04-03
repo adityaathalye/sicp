2beb08d70de1b246794a280aeea21ed626e59b7a
lambdadi <lambdadi@gmail.com>
2013-04-03 19:44:22 +0530

    Perverse  application of lambda

---
+++ chapter1/ex1-34-perverse-f-f.scm
@@ -0,0 +1,14 @@
+; Ex. 1.34 Perverse (f f)
+
+(define (f g)
+    (g 2))
+    
+;(f square)
+; ;Value: 6
+
+
+;(f (lambda (z) (* z (+ z 1))))
+; ;Value: 6
+
+; (f f)
+; ;The object 2 is not applicable.
