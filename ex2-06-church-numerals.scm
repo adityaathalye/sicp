6837f982bd0c02aa2fda4ff2eab6a8a6ae6c046e
lambdadi <lambdadi@gmail.com>
2013-04-16 12:52:15 +0530

    Deduced add from procedures add-1, one, and two
    With a clue from Chandra Sivaraman's post where I stopped reading after
    the point where he says:
    "; In retrospect, looking at the definition of add-1 above should
    ; have provided an instant clue as to Church's scheme."
    http://cdfrag.wordpress.com/2013/04/14/sicp-exercise-2-6/

--- ex2-06-church-numerals.scm
+++ ex2-06-church-numerals.scm
@@ -12,7 +12,20 @@
 (define two
   (lambda (f) (lambda (x) (f (f x)))))
 
-  
+(define three
+  (lambda (f) (lambda (x) (f (f (f x))))))
+
+(define (add m n)
+  (lambda (f) (lambda (x) ((m f) 
+                           ((n f) x)))))
+
+; (((add two two) square) 2)
+; ;Value: 65536
+
+; (square (square (square (square 2))))
+; ;Value: 65536
+
+                           
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Defining `one' (by trying to add-1 to zero by hand)
 
@@ -57,3 +70,41 @@
 
 ; ; ; HENCE we can directly define `two' as
 ; (lambda (f) (lambda (x) (f (f x))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Arriving at a general way to add two values:
+
+; Suppose add-1 n is rewritten as:
+; (define (add one n)
+  ; (lambda (f) (lambda (x) ((one f) ((n f) x)))))
+
+; (define (add one n)
+  ; (lambda (f) (lambda (x) (((lambda (f) 
+                               ; (lambda (x) (f x)))
+                             ; f)
+                            ; ((n f) x)))))
+
+; (define (add one n)
+  ; (lambda (f) (lambda (x) ((lambda (x) (f x)) ((n f) x)))))
+  
+; (define (add one n)
+  ; (lambda (f) (lambda (x) (f ((n f) x)))))
+  
+; Which is the same as the definition for add-1 n 
+
+; Likewise (add-2 n) and (add two n) are exactly similar:
+
+; (define (add two n)
+  ; (lambda (f) (lambda (x) ((two f) ((n f) x)))))
+
+; (define (add-2 n)
+  ; (lambda (f) (lambda (x) ((f (f ((n f) x)))))))
+
+  
+; This can be generalised to arrive at (add m n).
+
+; (define (add m n)
+  ; (lambda (f) (lambda (x) ((m f) 
+                           ; ((n f) x)))))
+
